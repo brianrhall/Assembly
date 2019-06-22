2828b356825ad20ceb19a0f63be587c6712308f0
brianrhall <brianrhall@brianrhall.net>
2019-06-22 16:29:13 -0400

    updated addressing method to match other versions

--- Chapter_4/Program 4.4/x86_64/Program_4.4_MASM.asm
+++ Chapter_4/Program 4.4/x86_64/Program_4.4_MASM.asm
@@ -11,14 +11,15 @@ array DWORD 1, 2, 3, 4
 _main PROC
 
 ; Load using byte offsets
-mov eax, array
-mov ebx, [array + 4]
+lea rsi, array
+mov eax, DWORD PTR [rsi]
+mov ebx, DWORD PTR [rsi + 4]
 
 ; Save using indices
 mov rdx, 2
-mov [array + rdx * 4], 10
+mov DWORD PTR [rsi + rdx * 4], 10
 mov rdx, 3
-mov [array + rdx * 4], 20
+mov DWORD PTR [rsi + rdx * 4], 20
 
 xor rcx, rcx
 call ExitProcess
